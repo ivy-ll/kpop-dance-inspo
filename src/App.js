babf4676b5671425557a5859eea947589af3133e
ivy-ll <ivy.lin128@gmail.com>
2025-02-01 12:51:54 -0800

    connecting frontend to backend

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const IntroDesc = () => {
 const fetchDance = async (groupType) => {
   try {
     let route = '';
-    const apiUrl = 'flask-production-e3ff.up.railway.app';
+    const apiUrl = 'https://kpop-dance-inspo-production.up.railway.app/';
 
     if (groupType === 'gg') {
       route = `${apiUrl}/api/getRandDanceGG`;
